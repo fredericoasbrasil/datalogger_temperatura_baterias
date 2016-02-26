5861b47fa4cfc223ea8fb22d0db1eff3b71b2efe
fredericoasbrasil <frederico.brasil@cear.ufpb.br>
2016-02-25 22:02:26 -0300

    DHT11
    Incluído código para ler temperatura utilizando sensores DHT11, apenas
    para teste pois devem ser substituidos por LM35 ou DBS32.
    
    Inclui desenho no Fritzing.

--- datalog_temp_baterias/datalog_temp_baterias.ino
+++ datalog_temp_baterias/datalog_temp_baterias.ino
@@ -4,13 +4,22 @@
 #include <SD.h>
 #include<SPI.h> 
 
+#include "DHT.h"
+
+//Define portas analógicas nas quais os DHTs estão ligados, no caso as analógicas 1 e 2
+#define dhtA A1
+#define dhtB A2
+#define DHTTYPE DHT11 // Tipo do sensor: DHT 11
+
+DHT sensorA(dhtA, DHTTYPE);
+DHT sensorB(dhtB, DHTTYPE);
+
 RTC_DS1307 RTC;
 int chipSelect = 10; //pino chipSelect (CS) ligado na porta 10
 File cartaoSD;
 
 //Portas dos sensores de temperatura
-int sensorA = 1;
-int sensorB = 2;
+
 String dadosSensores = "";
 
 String getDataHora(){
@@ -33,11 +42,14 @@ String getDataHora(){
 }
 
 
-String getTemperatura(int portaSensor){
-  long randomNumber;
-  
-  String temperatura = " - ";
-  return temperatura;
+String getTemperatura(DHT sensor){
+  float t = sensor.readTemperature();
+  if (isnan(t)) 
+  {
+    return String("ERRO NA LEITURA DE TEMP - VERIFIQUE SENSOR");
+  } else{ 
+  return String(t);
+  }
 }
 
 void gravaDados(File cartao, String dados ){
@@ -57,6 +69,8 @@ void setup () {
     Serial.begin(9600);
     Wire.begin();
     RTC.begin();
+    sensorA.begin();
+    sensorB.begin();
     //Ajusta a data/hora do Clock com a data/hora em que o codigo foi compilado
     RTC.adjust(DateTime(__DATE__, __TIME__));
     
@@ -83,6 +97,6 @@ void loop () {
     dadosSensores += getTemperatura(sensorB);
     Serial.println(dadosSensores);
     gravaDados(cartaoSD, dadosSensores);
-    delay(1000);//A leitura sera repetida cada 1 segundo
+    delay(500);//A leitura sera repetida cada 1 segundo
     dadosSensores = "";
 }
